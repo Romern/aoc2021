07c5a50d56071e975d363e6f487b2dc4e9546dc7
Roman Karwacik <roman.karwacik@rwth-aachen.de>
2021-12-14 10:38:08 +0100

    cleaned up code

--- 14/code.py
+++ 14/code.py
@@ -1,4 +1,5 @@
 from parse import parse
+from collections import Counter
 
 def parse_data(input):
 	polymer_template, rules = input.split("\n\n")
@@ -8,33 +9,21 @@ def parse_data(input):
 
 def part1(data, steps):
 	polymer_template, rules = data
-	polymer_amounts = dict()
+	polymer_amounts = Counter()
 	for i in range(len(polymer_template)-1):
-		if not polymer_amounts.get(polymer_template[i]+polymer_template[i+1]):
-	 		polymer_amounts[polymer_template[i]+polymer_template[i+1]] = 0
-		polymer_amounts[polymer_template[i]+polymer_template[i+1]] +=1
+		polymer_amounts[polymer_template[i:i+2]] +=1
 	for step in range(1,steps+1):
-		new_amounts = dict()
-		single_amounts = dict()
+		new_amounts = Counter()
+		single_amounts = Counter()
 		for pair, count in polymer_amounts.items():
 			if not rules.get(pair):
 				continue
-			if not new_amounts.get(pair[0]+rules[pair]):
-				new_amounts[pair[0]+rules[pair]] = 0
-			if not new_amounts.get(rules[pair]+pair[1]):
-				new_amounts[rules[pair]+pair[1]] = 0
-			if not single_amounts.get(pair[0]):
-				single_amounts[pair[0]] = 0
-			if not single_amounts.get(rules[pair]):
-				single_amounts[rules[pair]] = 0
-
 			new_amounts[pair[0]+rules[pair]] += count
 			new_amounts[rules[pair]+pair[1]] += count
 			single_amounts[pair[0]] += count
 			single_amounts[rules[pair]] += count
 		polymer_amounts = new_amounts
 		print(f"Step {step}")
-#		print(f"Step {step}: {polymer_amounts}")
 	keys = {l for s in polymer_amounts for l in s}
 	return sorted(single_amounts.values())[-1] - sorted(single_amounts.values())[0]
 
